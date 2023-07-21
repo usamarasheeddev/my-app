fa276bd862301c3e979918b8677ede29b1a8efb2
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2023-07-21 23:16:39 +0500

    add new product to db

--- src/pages/api/products/addNew.js
+++ src/pages/api/products/addNew.js
@@ -1,21 +1,21 @@
 import { Product } from "../../../../Schema/productModel"
 const { default: connectDb } = require("../../../../utils/Db")
 
-export default async function addProduct(req,res){
+export default async function addProduct(req, res) {
 
+    const { name, category, price } = req.body
 
-    
-    try {
-        await connectDb()
-       await Product.create({
-            name:'macbook',
-            price:900,
-            category:'laptop'
-        })
-        res.status(200).json({success:true,message:"Product created successfully"})
-    } catch (error) {
-        console.log(error)
-        res.status(400)
-        
+        try {
+            await connectDb()
+            await Product.create({
+                name,
+                price,
+                category
+            })
+            res.status(200).json({ success: true, message: "Product created successfully" })
+        } catch (error) {
+            console.log(error)
+            res.status(400)
+
+        }
     }
-}
