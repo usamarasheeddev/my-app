cc2b56122731d1ee8f95303d7fcf06276c8b5321
Usama Rasheed <65440307+usama2188@users.noreply.github.com>
2023-07-23 00:18:13 +0500

    static and dynamic routes

--- src/app/users/[id]/page.js
+++ src/app/users/[id]/page.js
@@ -1,17 +1,42 @@
 import React, { Suspense } from 'react'
 import { fetchUser } from '../../../../utils/users'
 import Post from './posts'
+import { notFound } from 'next/navigation'
 
-export const generateMetadata =async({params})=> {
-    const user = await fetchUser(params.id)
-    return {title:`${user.name}'s profile `}
+
+//disable all dynamic routes
+// export const dynamicParams=false
+
+//Static Rendring
+export const generateStaticParams = async () => {
+    return [
+        {
+            id: "1"
+        },
+        {
+            id: "2"
+        },
+        {
+            id: "3"
+        },
+    ]
+    //now it will render only ids array statically
 }
 
 
+//Changing metadata 
+export const generateMetadata = async ({ params }) => {
+    const user = await fetchUser(params.id)
+    return { title: `${user.name}'s profile ` }
+}
+
 export default async function page({ params }) {
+    const user = await fetchUser(params.id)
+    if(!user.id){
+        return notFound()
+    }
 
     // console.log(params.id)
-    const user = await fetchUser(params.id)
     return (
         <div>
 
